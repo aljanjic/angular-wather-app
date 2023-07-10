dcaeaacd40c3445017c087c0ab58aad82891dd84
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-10 20:50:00 +0200

    Creating app template and setting up environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+
   title = 'angular-weather-app';
+  cityControl: FormControl;
+
+  cities = ['London', 'Paris', 'Moscow', 'New York', 'Karachi', 'Sydney']
+
+
+  constructor(private router: Router){
+    this.cityControl = new FormControl('');
+    this.cityControl.valueChanges.subscribe(
+      (value) => {
+        this.router.navigate([value])
+      }
+    )
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+  ngOnDestroy(): void {}
+
+
+
+
+
+
 }
