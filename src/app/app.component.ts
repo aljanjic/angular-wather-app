aaa9f3e5a88750130ddff65f6e4c4ab2c6f60182
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-12 10:07:03 +0200

    Adding new cities to the list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'angular-weather-app';
   cityControl: FormControl;
 
-  cities = ['London', 'Paris', 'Moscow', 'New York', 'Karachi', 'Sydney']
+  cities = ['London', 'Paris', 'Moscow', 'New York', 'Karachi', 'Sydney', 'Bratislava', 'Banja Luka']
 
 
   constructor(
