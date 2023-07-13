60cafc3f0b74306f6c555156cc07f768de13cba0
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-13 21:43:09 +0200

    Removing comment

--- src/app/weather-app.service.ts
+++ src/app/weather-app.service.ts
@@ -10,19 +10,6 @@ export class WeatherAppService {
 
   constructor(private http: HttpClient) { }
 
-  // getWeatherForCity(city: string): Observable <any> {
-  //   const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=695ed9f29c4599b7544d0db5c211d499`;
-  //    return this.http.get(path).pipe(
-  //     map(rawData => ({
-  //       ...rawData,
-  //       // image: `https://openweathermap.org/img/wn/10d@2x.png`
-  //       image: `https://openweathermap.org/img/wn/${rawData.weather[0].icon}@2x.png`
-  //     }
-      
-  //     ))
-  //   );
-  // }
-
 
   getWeatherForCity(city: string): Observable<WeatherResponse> {
     const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=695ed9f29c4599b7544d0db5c211d499`;
@@ -41,4 +28,3 @@ interface WeatherResponse {
   weather: {icon: string}[];
   // Include any other properties you may be using from the data
 }
-// https://openweathermap.org/img/wn/10d@2x.png
