3627ccff663c52e007e7d11f514748e3632d7f3a
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-13 21:41:28 +0200

    Eanbling incon functionality

--- src/app/weather-app.service.ts
+++ src/app/weather-app.service.ts
@@ -10,16 +10,35 @@ export class WeatherAppService {
 
   constructor(private http: HttpClient) { }
 
-  getWeatherForCity(city: string): Observable <any> {
+  // getWeatherForCity(city: string): Observable <any> {
+  //   const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=695ed9f29c4599b7544d0db5c211d499`;
+  //    return this.http.get(path).pipe(
+  //     map(rawData => ({
+  //       ...rawData,
+  //       // image: `https://openweathermap.org/img/wn/10d@2x.png`
+  //       image: `https://openweathermap.org/img/wn/${rawData.weather[0].icon}@2x.png`
+  //     }
+      
+  //     ))
+  //   );
+  // }
+
+
+  getWeatherForCity(city: string): Observable<WeatherResponse> {
     const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=695ed9f29c4599b7544d0db5c211d499`;
-     return this.http.get(path).pipe(
-      map(rawData => ({
-        ...rawData,
-        // image: `https://openweathermap.org/img/wn/10d@2x.png`
-        // image: `https://openweathermap.org/img/wn/${rawData.weather[0].icon}@2x.png`
-      }))
+    return this.http.get<WeatherResponse>(path).pipe(
+      map((data: WeatherResponse) => ({
+        ...data,
+        image: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+      })),
     );
   }
+
 }
 
+
+interface WeatherResponse {
+  weather: {icon: string}[];
+  // Include any other properties you may be using from the data
+}
 // https://openweathermap.org/img/wn/10d@2x.png
