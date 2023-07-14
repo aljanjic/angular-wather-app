537e5044c7e39c696ad8c2097e52aecacb2b44c7
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-14 09:24:42 +0200

    Adding abilitiy to select a country

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -10,34 +12,47 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AppComponent implements OnInit, OnDestroy {
 
-
   title = 'angular-weather-app';
+
+  countryControl: FormControl;
   cityControl: FormControl;
 
-  cities = ['London', 'Paris', 'Moscow', 'New York', 'Karachi', 'Sydney', 'Bratislava', 'Nitra', 'Banja Luka', 'Doboj']
+  cities$: Observable<string>;
 
+  // cities = ['London', 'Paris', 'Moscow', 'New York', 'Karachi', 'Sydney', 'Bratislava', 'Nitra', 'Banja Luka', 'Doboj']
+  countries = [
+    {
+      name: 'Bosnia and Hercegovina',
+      cities: ['Banja Luka', 'Doboj', 'Sarajevo', 'Mostar', 'Tuzla']
+    },
+
+    {
+      name: 'Slovakia',
+      cities: ['Bratislava', 'Nitra', 'Kosice', 'Banská Bystrica']
+    },
+
+  ];
 
   constructor(
     private route: ActivatedRoute,
     private router: Router){
-    this.cityControl = new FormControl('');
-
   }
 
   ngOnInit(): void {
+    this.cityControl = new FormControl('');
     this.cityControl.valueChanges.subscribe(
       (value) => {
         this.router.navigate([value])
       }
     )
+    this.countryControl = new FormControl('')
 
+    this.cities$ = this.countryControl.valueChanges.pipe(
+
+      map(country => country.cities)
+    )
   }
 
   ngOnDestroy(): void {}
-
-
-
-
-
 
 }
